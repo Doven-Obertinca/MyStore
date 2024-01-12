bde5df93fa73e8740b4dce6163448927affb9d5b
Doven Obertinca <doven.obertinca@gmail.com>
2024-01-12 16:38:55 +0100

    adding basket migration adding basket to the database

--- ReStore/client/src/app/api/agent.ts
+++ ReStore/client/src/app/api/agent.ts
@@ -1,4 +1,4 @@
-/* eslint-disable @typescript-eslint/ban-types */
+
 
 
 import axios, { AxiosError, AxiosResponse } from "axios";
@@ -66,9 +66,8 @@ axios.interceptors.response.use(async response => {
 // };
 const requests = {
   get: (url: string, params?: URLSearchParams) => axios.get(url, {params}).then(responseBody),
-  // eslint-disable-next-line @typescript-eslint/ban-types
-  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-  put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
+  post: (url: string, body: object) => axios.post(url, body).then(responseBody),
+  put: (url: string, body: object) => axios.put(url, body).then(responseBody),
   delete: (url: string) => axios.delete(url).then(responseBody),
   postForm: (url: string, data: FormData) => axios.post(url, data, {
       headers: {'Content-type': 'multipart/form-data'}

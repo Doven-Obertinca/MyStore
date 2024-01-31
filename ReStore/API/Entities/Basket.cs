a57c302383d8453791662f39061a8b872fa4cab6
Doven Obertinca <doven.obertinca@gmail.com>
2024-01-31 13:33:31 +0100

    adding basket and basket table that now we can see or ordered products

--- ReStore/API/Entities/Basket.cs
+++ ReStore/API/Entities/Basket.cs
@@ -1,37 +1,53 @@
 using API.Controllers;
 using API.DTOs;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-namespace API.Entities;
-
-public class Basket
+namespace API.Entities
 {
-    public int Id { get; set; }
-    public string BuyerId { get; set; }
-    public List<BasketItem> Items { get; set; } = new List<BasketItem>();
-
-    public void AddItem(Product product, int quantity)
+    public class Basket
     {
-        if (Items.All(item => item.ProductId != product.Id))
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public List<BasketItem> Items { get; set; } = new List<BasketItem>();
+
+        public void AddItem(Product product, int quantity)
         {
-            Items.Add(new BasketItem { Product = product, Quantity = quantity });
-            return;
+            if (Items.All(item => item.ProductId != product.Id))
+            {
+                Items.Add(new BasketItem { Product = product, Quantity = quantity });
+                return;
+            }
+
+            var existingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
+            if (existingItem != null) existingItem.Quantity += quantity;
         }
 
-        var existingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
-        if (existingItem != null) existingItem.Quantity += quantity;
-    }
+        public void RemoveItem(int productId, int quantity = 1)
+        {
+            var item = Items.FirstOrDefault(basketItem => basketItem.ProductId == productId);
+            if (item == null) return;
+            item.Quantity -= quantity;
+            if (item.Quantity == 0) Items.Remove(item);
+        }
 
-    public void RemoveItem(int productId, int quantity = 1)
-    {
-        var item = Items.FirstOrDefault(basketItem => basketItem.ProductId == productId);
-        if (item == null) return;
-        item.Quantity -= quantity;
-        if (item.Quantity == 0) Items.Remove(item);
-    }
+        public BasketDto MapBasketToDto()
+        {
+            return new BasketDto
+            {
+                Id = this.Id,
+                BuyerId = this.BuyerId,
+                Items = this.Items.Select(item => new BasketItemDto
+                {
+                    ProductId = item.Product.Id,
+                    Name = item.Product.Name, // Add product name property
+                    Price = item.Product.Price, // Add product price property
+                    Quantity = item.Quantity
+                }).ToList()
+            };
+        }
 
-    internal ActionResult<BasketDto> MapBasketToDto()
-    {
-        throw new NotImplementedException();
     }
 }
